a3403b629ef7be730dbd93ee260fc91a8d732f03
Serjo85 <bobrosebob07@gmail.com>
2022-02-23 19:23:15 +0300

    Реализована маршрутизация по умолчанию.

--- WebStore/Program.cs
+++ WebStore/Program.cs
@@ -2,6 +2,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 
+app.UseRouting();
+
 app.MapGet("/", () => app.Configuration["ServerGreetings"]);
+
+app.MapDefaultControllerRoute();
 
 app.Run();
