ba56d8affdc444f63c8dd0cc9bd5bccb7f1bcd6c
Serjo85 <bobrosebob07@gmail.com>
2022-02-23 20:03:41 +0300

    Добавлено указание маршрута для контроллера.

--- WebStore/Program.cs
+++ WebStore/Program.cs
@@ -7,5 +7,7 @@ app.UseRouting();
 app.MapGet("/", () => app.Configuration["ServerGreetings"]);
 
 app.MapDefaultControllerRoute();
-
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 app.Run();
