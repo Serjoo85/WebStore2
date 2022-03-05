9647469c65938cd30f0330559c5ff0c03aa82077
Serjo85 <bobrosebob07@gmail.com>
2022-03-05 16:24:42 +0300

    Вывод детальной информации о сотруднике

---
+++ WebStore/Controllers/EmployeesController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebStore.Models;
+
+namespace WebStore.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private static readonly List<Employee> __Employees = new List<Employee>
+        {
+            new Employee { Id =1, Age = 21, LastName = "Иванов", FirstName = "Иван", Patronymic = "Иванович", Position = "Менеджер читсоты", Salary = 10000},
+            new Employee { Id =2, Age = 22, LastName = "Пётров", FirstName = "Пётр", Patronymic = "Петрович", Position = "Менеджер читсоты", Salary = 10000},
+            new Employee { Id =3, Age = 23, LastName = "Сидоров", FirstName = "Сидр", Patronymic = "Сидорович", Position = "Менеджер читсоты", Salary = 10000},
+        };
+
+        public IActionResult Index()
+        {
+            return View(__Employees);
+        }
+
+        public IActionResult Detailes(int Id)
+        {
+            var employee = __Employees.FirstOrDefault(e => e.Id == Id);
+            if(employee == null)
+                return NotFound();
+            return View(employee);
+        }
+    }
+}
--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -1,36 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using WebStore.Models;
 
 namespace WebStore.Controllers
 {
     public class HomeController : Controller
     {
-        private static readonly List<Employee> __Employees = new List<Employee>
-        {
-            new Employee { Id =1, Age = 21, LastName = "Иванов", FirstName = "Иван", Patronymic = "Иванович", Position = "Менеджер читсоты", Salary = 10000},
-            new Employee { Id =2, Age = 22, LastName = "Пётров", FirstName = "Пётр", Patronymic = "Петрович", Position = "Менеджер читсоты", Salary = 10000},
-            new Employee { Id =3, Age = 23, LastName = "Сидоров", FirstName = "Сидр", Patronymic = "Сидорович", Position = "Менеджер читсоты", Salary = 10000},
-        };
-
         private readonly IConfiguration _configuration;
 
         public HomeController(IConfiguration configuration)
         {
             _configuration = configuration;
         }
-        public IActionResult Employees()
-        {
-            return View(__Employees);
-        }
 
         public IActionResult Index()
         {
             return View();
-        }
-
-        public IActionResult PersonalCard(int id)
-        {
-            return View(__Employees[id - 1]);
         }
     }
 }
