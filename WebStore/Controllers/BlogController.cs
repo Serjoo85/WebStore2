f1e12ad541c96b8f80c2785778fbbc67cfe06936
Serjo85 <bobrosebob07@gmail.com>
2022-03-06 23:33:33 +0300

    Добавлен контроллер блога с редставлением

---
+++ WebStore/Controllers/BlogController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.Controllers
+{
+    public class BlogController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult SingleBlog()
+        {
+            var x = View();
+            return x;
+        }
+    }
+}
