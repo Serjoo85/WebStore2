f35c648d9960791dd482f351b79b6abdcbf61d92
Serjo85 <bobrosebob07@gmail.com>
2022-03-08 16:20:12 +0300

    Рализация сервиса управления сотрудниками

--- WebStore/Controllers/EmployeesController.cs
+++ WebStore/Controllers/EmployeesController.cs
@@ -5,12 +5,7 @@ namespace WebStore.Controllers
 {
     public class EmployeesController : Controller
     {
-        private static readonly List<Employee> __Employees = new List<Employee>
-        {
-            new Employee { Id =1, Age = 21, LastName = "Иванов", FirstName = "Иван", Patronymic = "Иванович", Position = "Менеджер читсоты", Salary = 10000},
-            new Employee { Id =2, Age = 22, LastName = "Пётров", FirstName = "Пётр", Patronymic = "Петрович", Position = "Менеджер читсоты", Salary = 10000},
-            new Employee { Id =3, Age = 23, LastName = "Сидоров", FirstName = "Сидр", Patronymic = "Сидорович", Position = "Менеджер читсоты", Salary = 10000},
-        };
+
 
         public IActionResult Index()
         {
