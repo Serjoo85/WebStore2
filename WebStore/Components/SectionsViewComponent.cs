3ac4ddf4830bd5ec5ab7d0af431a7b2e5dd859a3
Serjo85 <bobrosebob07@gmail.com>
2022-03-15 20:51:59 +0300

    Визуальный компонент секций

--- WebStore/Components/SectionsViewComponent.cs
+++ WebStore/Components/SectionsViewComponent.cs
@@ -1,8 +1,44 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebStore.Services.Interfaces;
+using WebStore.ViewModels;
 
 namespace WebStore.Components;
 
 public class SectionsViewComponent : ViewComponent
 {
-    public IViewComponentResult Invoke() => View();
+    private readonly IProductData _productData;
+
+    public SectionsViewComponent(IProductData productData) => _productData = productData;
+
+    public IViewComponentResult Invoke()
+    {
+        var sections = _productData.GetSections();
+        var parentSections = sections.Where(s => s.ParentId is null);
+        var parentSectionViewModels = parentSections.Select(ps =>
+            new SectionViewModel()
+            {
+                Id = ps.Id,
+                Name = ps.Name,
+                Order = ps.Order,
+            })
+            .ToList();
+
+        foreach (var parentSection in parentSectionViewModels)
+        {
+            var childSections = sections.Where(s => s.ParentId == parentSection.Id).ToList();
+
+            foreach (var section in childSections)
+            {
+                parentSection.ChildSections.Add(new SectionViewModel()
+                {
+                    Id = section.Id,
+                    Name = section.Name,
+                    Order = section.Order,
+                });
+            }
+
+            parentSection.ChildSections.Sort((a,b) => Comparer<int>.Default.Compare(a.Order, b.Order));
+        }
+        return View(parentSectionViewModels);
+    }
 }
