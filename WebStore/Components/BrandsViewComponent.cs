719c9b1efd00e31d37260a1b7bac629db555b4f7
Serjo85 <bobrosebob07@gmail.com>
2022-03-15 23:27:46 +0300

    Визуальный компонент бренда

--- WebStore/Components/BrandsViewComponent.cs
+++ WebStore/Components/BrandsViewComponent.cs
@@ -1,8 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebStore.Services.Interfaces;
+using WebStore.ViewModels;
 
 namespace WebStore.Components;
 
 public class BrandsViewComponent : ViewComponent
 {
-    public IViewComponentResult Invoke() => View();
+    private readonly IProductData _productData;
+
+    public BrandsViewComponent(IProductData productData)
+    {
+        _productData = productData;
+    }
+    public IViewComponentResult Invoke()
+    {
+        var brands = _productData.GetBrands();
+
+        var brandViewModels = brands.Select(b => new BrandViewModel()
+            {
+                Id = b.Id,
+                Name = b.Name,
+            })
+            .ToList();
+
+
+        return View(brandViewModels);
+    }
 }
