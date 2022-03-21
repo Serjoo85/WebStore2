ae89c597b1b9a3b1972da5423fd03440e50c4776
Serjo85 <bobrosebob07@gmail.com>
2022-03-21 19:34:07 +0300

    Строка подключения

---
+++ WebStore.DAL/Context/WebStoreDB.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using WebStore.Domain.Entities;
+
+namespace WebStore.DAL.Context;
+
+public class WebStoreDb : DbContext
+{
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Section> Sections { get; set; }
+    public DbSet<Brand> Brands { get; set; }
+
+    public WebStoreDb(DbContextOptions<WebStoreDb> options) : base(options)
+    {
+        
+    }
+
+    // Пример fluentApi для настройки базы даных.
+    //protected override void OnModelCreating(ModelBuilder model)
+    //{
+    //    base.OnModelCreating(model);
+    //    model.Entity<Brand>()
+    //        .HasMany(brand => brand.Products)
+    //        .WithOne(product => product.Brand)
+    //        .OnDelete(DeleteBehavior.Cascade);
+    //}
+}
