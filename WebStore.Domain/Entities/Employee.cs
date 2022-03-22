7592d835bbc24caa2a7a74c330bbd111d8f6f54f
Serjo85 <bobrosebob07@gmail.com>
2022-03-22 18:26:29 +0300

    Подготовка сущности Employee

--- WebStore.Domain/Entities/Employee.cs
+++ WebStore.Domain/Entities/Employee.cs
@@ -1,16 +1,24 @@
-﻿using WebStore.Domain.Entities.Base;
+﻿using System.ComponentModel.DataAnnotations;
+using WebStore.Domain.Entities.Base;
 
 namespace WebStore.Domain.Entities;
 
 public class Employee : Entity
 {
+    [Required]
     public string LastName { get; set; } = null!;
+
+    [Required]
     public string FirstName { get; set; } = null!;
+
     public string? Patronymic { get; set; }
 
+    [Required]
     public int Age { get; set;}
 
+    [Required]
     public string Position { get; set; } = null!;
 
+    [Required]
     public decimal Salary { get; set; }
 }
