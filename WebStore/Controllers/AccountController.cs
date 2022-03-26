3c0ec2e885333c20aea6623b039a00fb2232dc5c
Serjo85 <bobrosebob07@gmail.com>
2022-03-26 21:33:25 +0300

    Заготовка контроллера AccountController

---
+++ WebStore/Controllers/AccountController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.Controllers;
+
+public class AccountController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
