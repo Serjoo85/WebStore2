b88e94762023d5affaddec11854d1e61e83aaf37
Serjo85 <bobrosebob07@gmail.com>
2022-04-05 19:35:56 +0300

    Класс InMemmory отмечен атрибутом Obsolete

--- WebStore/Services/InMemoryProductData.cs
+++ WebStore/Services/InMemoryProductData.cs
@@ -5,11 +5,22 @@ using WebStore.Services.Interfaces;
 
 namespace WebStore.Services;
 
+[Obsolete("Будет удаён в ближайшее время", true)]
 public class InMemoryProductData : IProductData
 {
     public IEnumerable<Section> GetSections() => TestData.Sections;
+    public Section? GetSectionById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
 
     public IEnumerable<Brand> GetBrands() => TestData.Brands;
+    public Brand? GetBrandById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
     public IEnumerable<Product> GetProducts(ProductFilter? filter = null)
     {
         IEnumerable<Product> query = TestData.Products;
@@ -25,5 +36,10 @@ public class InMemoryProductData : IProductData
             query = query.Where(x => x.BrandId == brandId);
 
         return query;
+    }
+
+    public Product? GetProductById(int id)
+    {
+        throw new NotImplementedException();
     }
 }
