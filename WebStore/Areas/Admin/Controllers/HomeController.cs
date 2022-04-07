56793b6dfc0939a2007ce14dd8cf4a9f04a3e0a3
Serjo85 <bobrosebob07@gmail.com>
2022-04-07 20:30:41 +0300

    Загатовка структуры MVC для области администратора.

--- WebStore/Areas/Admin/Controllers/HomeController.cs
+++ WebStore/Areas/Admin/Controllers/HomeController.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.Operations;
 
 namespace WebStore.Areas.Admin.Controllers;
 
 //[Area("Admin")]
+//[Authorize]
 public class HomeController : Controller
 {
-
+    public IActionResult Index() => View();
 }
