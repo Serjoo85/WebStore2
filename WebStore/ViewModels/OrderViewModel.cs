c350606548f94d85c39274912a65554f44825510
Serjo85 <bobrosebob07@gmail.com>
2022-04-11 21:15:57 +0300

    Логика формирования заказа в контроллере корзины.

--- WebStore/ViewModels/OrderViewModel.cs
+++ WebStore/ViewModels/OrderViewModel.cs
@@ -1,6 +1,15 @@
-﻿namespace WebStore.ViewModels;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebStore.ViewModels;
 
 public class OrderViewModel
 {
-    
+    [Required]
+    [MaxLength(200)]
+    public string Address { get; set; } = null!;
+    [Required]
+    [MaxLength(200)]
+    public string Phone { get; set; } = null!;
+    [Required]
+    public string? Description { get; set; }
 }
