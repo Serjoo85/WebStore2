6a63b03a782377171288f581994cd5b044d3f4cc
Serjo85 <bobrosebob07@gmail.com>
2022-04-17 20:54:44 +0300

    Загатовка контроллера и клиента

---
+++ Services/WebStore.WebAPI.Clients/Employees/EmployeesClient.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.Logging;
+using WebStore.Domain.Entities;
+using WebStore.Interfaces.Services;
+using WebStore.WebAPI.Clients.Base;
+
+namespace WebStore.WebAPI.Clients.Employees;
+
+public class EmployeesClient : BaseClient, IEmployeesData
+{
+    private readonly ILogger _logger;
+
+    public EmployeesClient(HttpClient client, ILogger logger) : base(client, "api/employees")
+    {
+        _logger = logger;
+    }
+
+    public IEnumerable<Employee> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Employee> GetById(int id, CancellationToken token)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<int> Add(Employee employee, CancellationToken token)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task Edit(Employee employee, CancellationToken token)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task Delete(int id, CancellationToken token)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Services/WebStore.WebAPI/Controllers/EmployeesApiController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebStore.Interfaces.Services;
+
+namespace WebStore.WebAPI.Controllers;
+
+[ApiController]
+[Route("api/employees")]
+public class EmployeesApiController : ControllerBase
+{
+    private readonly IEmployeesData _employeesData;
+    private readonly ILogger _logger;
+
+    public EmployeesApiController(IEmployeesData employeesData, ILogger logger)
+    {
+        _employeesData = employeesData;
+        _logger = logger;
+    }
+
+
+}
