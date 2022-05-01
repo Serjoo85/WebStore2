3c187e89f945476afd87c7519750f1e14d337a94
Serjo85 <bobrosebob07@gmail.com>
2022-05-01 15:20:34 +0300

    Добавлен консольный проект с реализацие работы с WebAPI сервисом продуктов.

---
+++ WebStore.ConsoleUI/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+using WebStore.WebAPI.Clients.Products;
+
+var configuration = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json")
+    .Build();
+
+var http = new HttpClient
+{
+    BaseAddress = new(configuration["WebAPI"])
+};
+
+var productsClient = new ProductsClient(http, null);
+
+Console.WriteLine("Ожидание запуска WebAPI");
+Console.ReadLine();
+
+foreach (var product in productsClient.GetProducts())
+{
+    Console.WriteLine("[{0}] {1}", product.Id, product.Name);
+}
+
+Console.ReadKey();
+
