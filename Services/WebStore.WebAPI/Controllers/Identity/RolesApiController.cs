a6b8ffeaa6d1cb37e7807cd652a6ea5d15159fe1
Serjo85 <bobrosebob07@gmail.com>
2022-05-04 19:06:24 +0300

    Проверка работоспособности контроллеров, добавлены тестовые методы.

--- Services/WebStore.WebAPI/Controllers/Identity/RolesApiController.cs
+++ Services/WebStore.WebAPI/Controllers/Identity/RolesApiController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebStore.DAL.Context;
+using WebStore.Domain.Entities.Identity;
 using WebStore.Interfaces;
 
 namespace WebStore.WebAPI.Controllers.Identity;
@@ -7,5 +11,19 @@ namespace WebStore.WebAPI.Controllers.Identity;
 [Route(WebApiAddresses.V1.Identity.Roles)]
 public class RolesApiController : ControllerBase
 {
+    private readonly ILogger<RolesApiController> _logger;
+    private readonly RoleStore<Role> _roleStore;
 
+    public RolesApiController(WebStoreDb db, ILogger<RolesApiController> logger)
+    {
+        _logger = logger;
+        _roleStore = new(db);
+    }
+
+    [HttpGet("all")]
+    public async Task<IActionResult> GetAllRole()
+    {
+        var roles = await _roleStore.Roles.ToArrayAsync();
+        return Ok(roles);
+    }
 }
--- Services/WebStore.WebAPI/Controllers/Identity/UserApiController.cs
+++ Services/WebStore.WebAPI/Controllers/Identity/UserApiController.cs
@@ -1,10 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebStore.DAL.Context;
+using WebStore.Domain.Entities.Identity;
 using WebStore.Interfaces;
 
 namespace WebStore.WebAPI.Controllers.Identity;
 
 [ApiController]
-[Route(WebApiAddresses.V1.Identity.Roles)]
+[Route(WebApiAddresses.V1.Identity.Users)]
 public class UserApiController : ControllerBase
 {
+    private readonly UserStore<User,Role, WebStoreDb> _userStore;
+    private readonly ILogger<UserApiController> _logger;
+
+    public UserApiController(WebStoreDb db, ILogger<UserApiController> logger)
+    {
+        _userStore = new(db);
+        _logger = logger;
+    }
+
+    [HttpGet("all")]
+    public async Task<IActionResult> GetAllUsers()
+    {
+        var users = await _userStore.Users.ToArrayAsync();
+        return Ok(users);
+    }
 }
