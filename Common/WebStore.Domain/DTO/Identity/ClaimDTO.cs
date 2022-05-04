f27fc4c6a0549faf01f4c592d6021b9e58c70db6
Serjo85 <bobrosebob07@gmail.com>
2022-05-04 19:27:10 +0300

    DTO модели для системы Identity.

---
+++ Common/WebStore.Domain/DTO/Identity/ClaimDTO.cs
@@ -0,0 +1,15 @@
+﻿using System.Security.Claims;
+
+namespace WebStore.Domain.DTO;
+
+public class ClaimDTO : UserDTO
+{
+    public IEnumerable<Claim> Claims { get; set; } = null!;
+}
+
+public class ReplaceClaimDTO : UserDTO
+{
+    public Claim Claim { get; set; } = null!;
+
+    public Claim NewClaim { get; set; } = null!;
+}
---
+++ Common/WebStore.Domain/DTO/Identity/UserDTO.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using WebStore.Domain.Entities.Identity;
+
+namespace WebStore.Domain.DTO;
+
+public class UserDTO
+{
+    public User User { get; set; } = null!;
+}
+
+public class AddLogginDTO : UserDTO
+{
+    public UserLoginInfo UserLoginInfo { get; set; } = null!;
+}
+
+public class PasswordHashDTO : UserDTO
+{
+    public string Hash { get; set; } = null!;
+}
+
+public class SetLockoutDTO : UserDTO
+{
+    public DateTimeOffset? LockoutEnd { get; set; }
+}
