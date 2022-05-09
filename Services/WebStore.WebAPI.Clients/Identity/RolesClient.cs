d0e68d2ae620c2a61f8a682ba8eefc3d799233d5
Serjo85 <bobrosebob07@gmail.com>
2022-05-09 16:06:10 +0300

    Заготовки клентов для User и Role

---
+++ Services/WebStore.WebAPI.Clients/Identity/RolesClient.cs
@@ -0,0 +1,12 @@
+﻿using WebStore.Interfaces;
+using WebStore.WebAPI.Clients.Base;
+
+namespace WebStore.WebAPI.Clients.Identity;
+
+public class RolesClient : BaseClient
+{
+    public RolesClient(HttpClient client) : base(client, WebApiAddresses.V1.Identity.Roles)
+    {
+        
+    }
+}
---
+++ Services/WebStore.WebAPI.Clients/Identity/UserClient.cs
@@ -0,0 +1,12 @@
+﻿using WebStore.Interfaces;
+using WebStore.WebAPI.Clients.Base;
+
+namespace WebStore.WebAPI.Clients.Identity;
+
+public class UserClient : BaseClient
+{
+    public UserClient(HttpClient client) : base(client, WebApiAddresses.V1.Identity.Users)
+    {
+        
+    }
+}
